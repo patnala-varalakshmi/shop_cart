35af93bb0b2a26775f49141b6325f67d5e6e4844
vara <varalakshmi@greycampus.com>
2019-09-11 15:05:17 +0530

    items table added

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,26 @@
+class ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+    end
+    
+    def new 
+
+    end
+
+    def create
+        @item = Item.new(item_params)
+        @item.save
+        redirect_to items_path
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    private
+
+        def item_params
+            params.require(:item).permit(:name, :price)
+        end
+end
